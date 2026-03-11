4b7b9b950c51c3095489ecb924121e582c9cf3a7
Swapnil Dhane <swapnildhane98@gmail.com>
2026-03-11 17:54:49 +0530

    my first employee task manager add using blazor app server

---
+++ Employee_Task_Manager/Employee_Task_Manager/Model/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Employee_Task_Manager.Model
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? name { get; set; }
+        public string? Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ Employee_Task_Manager/Employee_Task_Manager/Services/EmployeeService.cs
@@ -0,0 +1,36 @@
+﻿using Employee_Task_Manager.Model;
+using EmployeeTaskManager.Data;
+
+public class EmployeeService
+{
+    private readonly AppDbContext _context;
+
+    public EmployeeService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<Employee> GetEmployees()
+    {
+        return _context.Employees.ToList();
+    }
+
+    public void AddEmployee(Employee emp)
+    {
+        emp.Id = 0;   // Important fix
+
+        _context.Employees.Add(emp);
+        _context.SaveChanges();
+    }
+
+    public void DeleteEmployee(int id)
+    {
+        var emp = _context.Employees.Find(id);
+
+        if (emp != null)
+        {
+            _context.Employees.Remove(emp);
+            _context.SaveChanges();
+        }
+    }
+}
